05147c869ed9c3ca5c47cb9db8f26d7e6b556ac3
Lucas <frelon99@gmail.com>
2024-10-24 14:56:58 +0200

    premier commit composant addTask

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import './components/Input'
+import AddTask from './components/Input';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([
+    {id: 1, text: "Faire les courses", completed: false},
+    {id: 2, text: "Boire de l'eau", completed: false},
+    {id: 3, text: "Faire du sport", completed: false},
+  ]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const addTask = (newTaskText) => {
+  const newTask = {
+    id: tasks.length + 1,
+    text: newTaskText,
+    completed: false, 
+  };
+  setTasks([...tasks, newTask]);
+};
+
+	return (
+		<div>
+			<Input AddTask={addTask} />
+		</div>
+	);
 }
 
 export default App
---
+++ src/components/Input.jsx
@@ -0,0 +1,7 @@
+function AddTask() {
+    return (
+        <input type="submit" name="yourTask" placeholder="Add your task here"></input>
+    );
+}
+
+export default AddTask;
